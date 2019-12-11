536f7af768fdc4145f24068f717e89b7dc6d5fb3
Todd Oakley <oakley@ucsb.edu>
2019-12-11 08:41:10 -0800

    adding fasta2csv.pl

---
+++ LuciferaseTree_dNds/scripts/fasta2csv.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+use strict;
+use Bio::SeqIO;
+
+#This script converts a fasta sequence file into a comma delimited text file, e.g. to read into R
+#USAGE: fasta2csv.pl <fastafile>
+#It requires BioPerl's SeqIO package. Simply writes to screen. Use redirect > to write to a file.
+
+my $datafile = $ARGV[0];
+
+my $seq_in = Bio::SeqIO->new(-file => $datafile); 
+
+while ( my $seq = $seq_in->next_seq() ) {
+	print $seq->id.",";
+	my $theseq = $seq->seq;
+	my @residues = split //, $theseq;
+	my $csv = join ",", @residues;
+	print "$csv\n";
+}
