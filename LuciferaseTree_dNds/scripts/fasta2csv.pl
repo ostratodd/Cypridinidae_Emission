9929bcff6ced17f7fd86bc26f6c316baaf8d2349
Todd Oakley <oakley@ucsb.edu>
2019-12-19 14:11:05 -0800

    making mutational analysis more programmed

--- LuciferaseTree_dNds/scripts/fasta2csv.pl
+++ LuciferaseTree_dNds/scripts/fasta2csv.pl
@@ -6,15 +6,25 @@ use Bio::SeqIO;
 #USAGE: fasta2csv.pl <fastafile>
 #It requires BioPerl's SeqIO package. Simply writes to screen. Use redirect > to write to a file.
 # 
+#special use is that if sequence name contains __Lmax### use the ### for the last column for mutant analysis of color
 
 my $datafile = $ARGV[0];
 
 my $seq_in = Bio::SeqIO->new(-file => $datafile); 
+my $lmax;
+my @lmaxes;
 
 while ( my $seq = $seq_in->next_seq() ) {
+	if($seq->id =~ m/Lmax/) {
+		@lmaxes = split /Lmax/, $seq->id;
+		$lmax = $lmaxes[1];
+	}
 	print $seq->id.",";
 	my $theseq = $seq->seq;
 	my @residues = split //, $theseq;
 	my $csv = join ",", @residues;
-	print "$csv\n";
+	print "$csv";
+	if($lmax){
+		print ",".$lmax."\n";
+	}
 }
