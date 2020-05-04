5d104e8c531d5cb0d9a5d425f9ebcf432d174396
Todd Oakley <oakley@ucsb.edu>
2020-05-04 12:30:37 -0700

    fig 3 by genus
    separate by genus not locality

--- Figure3.R
+++ Figure3.R
@@ -7,4 +7,4 @@ quartz("Figure 3", 13, 3)
 fig3 <- ggplot(data=clean, aes(x=abbreviation, y=sgMax, fill=abbreviation)) + geom_boxplot() + geom_jitter() 
 
 
-fig3 + facet_grid(cols = vars(locality), scales = "free_x", switch = "x", space = "free")  + ylab("Lambda max (nm)") + xlab("Species") + scale_fill_manual(values=c("green", "green","#0092ff", "#0092ff", "#0092ff", "green","green", "#0092ff","#0092ff", "#0092ff","#0092ff", "green","green", "#0092ff","#0092ff", "#0092ff","#0092ff", "#0092ff", "#0092ff","#0092ff", "green","#0092ff", "#0092ff")) + theme(legend.position = "none")
+fig3 + facet_grid(cols = vars(genus), scales = "free_x", switch = "x", space = "free")  + ylab("Lambda max (nm)") + xlab("Species") + scale_fill_manual(values=c("#00a9ff", "#00a9ff","#007bff", "#007bff", "#007bff", "#00a9ff","#00a9ff", "#007bff","#007bff", "#007bff","#007bff", "#00a9ff","#00a9ff", "#007bff","#007bff", "#007bff","#007bff", "#007bff", "#007bff","#007bff", "#00a9ff","#007bff", "#007bff")) + theme(legend.position = "none")
