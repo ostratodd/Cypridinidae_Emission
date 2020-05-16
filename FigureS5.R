be87ef7f6800ffba87258e70b5121ca6727e43dc
Niko <nikohensley@gmail.com>
2020-05-15 21:51:01 -0700

    figure S5 finalized

--- FigureS5.R
+++ FigureS5.R
@@ -26,50 +26,48 @@ hyphy_dat <- merge(sel_dat,multihit,by= "site")
 
 #plot hyphy data together
 library(ggplot2); library(reshape2); library(gridExtra)
-coeff <- 1
-ggplot(data=hyphy_dat,aes(x=site)) + geom_line(aes(y=(1-p_value)),size=0.5) +
-  geom_hline(yintercept = 0.95,col="red",lty=2) +
-  geom_point(aes(y=two/coeff),size=0.5,alpha=0.5,color="blue") +
-  scale_y_continuous(name="1 - P-value",sec.axis = sec_axis(~.*coeff, name="Evidence ratio of Two MNS"),limits = c(0,400))
-
 df <- melt(hyphy_dat,id.vars="site",measure.vars=c("three","two"))
-ggplot(data=df,aes(x=site,y=value)) + geom_line() + facet_grid(.~variable)
 
-hyphy_cols <- c("blue","grey")
-p1 <- ggplot(data=hyphy_dat,aes(x=site)) + #geom_point(aes(y=(1-p_value)),size=0.5) +
-  geom_linerange(aes(x=site,ymax=(1-p_value),ymin=0,color=variable)) +
-  geom_hline(yintercept = 0.95,col="red",lty=2) + scale_color_manual(values=hyphy_cols) +
-  theme(legend.position = "none") + ylab("1 - P-value")
-p2 <- ggplot(data=df,aes(x=site)) + geom_line(aes(y=log(value),color=as.factor(variable)),size=0.5,alpha=0.75) +
-  geom_hline(yintercept = log(5),col="grey",lty=2) +
-  geom_hline(yintercept = log(2),col="grey",lty=2) +
-  theme(legend.position = "none") + ylab("Natural log (Evidence Ratio for MNS model)")
-
+hyphy_cols <- c("black","darkgrey")
 p1 <- ggplot(data=hyphy_dat,aes(x=site)) +
-  geom_hline(yintercept = 0.95,col="red",lty=2) + scale_color_manual(values=hyphy_cols) +
-  geom_point(aes(y=(1-p_value),color=variable),alpha=0.5) +
+  geom_hline(yintercept = 0.95,col="grey",lty=2) + scale_color_manual(values=hyphy_cols) +
+  geom_hline(yintercept = 0.975,col = "red", lty = 2) +
+  geom_point(aes(y=(1-p_value),color=variable),alpha=0.25) + scale_shape_manual(values=shap_hyphy) +
   geom_point(data=subset.data.frame(hyphy_dat[hyphy_dat$p_value <= 0.05,]),aes(y=(1-p_value),color=variable)) +
-  ylab("1 - P-value") + scale_y_continuous(limits=c(0.5,1)) + theme_bw() + theme(legend.position = "none", panel.border = element_blank(), panel.grid.major = element_blank(),
+  ylab("1 - P-value") + scale_y_continuous(limits=c(0.8,1)) + theme_bw() + theme(legend.position = "none", panel.border = element_blank(), panel.grid.major = element_blank(),
                                                                                  panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
-
+df_cols <- c("black","darkgrey")
 p2 <- ggplot(data=df,aes(x=site)) +
-  geom_vline(xintercept = c(207,211,406,435,102,142,189,41,261),lty=1,col="grey") +
+  geom_vline(xintercept = c(207,311,406),lty=1,col="goldenrod2",size=1.25,alpha=0.5) + #neutral
+  geom_vline(xintercept = c(435),lty=1,col="deeppink",size=1.25,alpha=0.5) + #purifying / negative
+  geom_vline(xintercept = c(102,142,189,41,261),lty=1,col="darkturquoise",size=1.25,alpha=0.5) +  #diversifying / positive
   geom_hline(yintercept = log(5),col="black",lty=2) +
   geom_hline(yintercept = log(2),col="black",lty=2) +
   geom_point(aes(y=log(value),color=as.factor(variable)),alpha=0.25) +
-  geom_point(data=subset.data.frame(df[df$value > 1,]),aes(y=log(value),color=as.factor(variable))) + ylab("Natural log (Evidence Ratio for MNS model)") +
+  geom_point(data=subset.data.frame(df[df$value > 1,]),aes(y=log(value),color=as.factor(variable))) +
+  ylab("Natural log (Evidence Ratio for MNS model)") + scale_color_manual(values=df_cols) +
   theme_bw() + theme(legend.position = "none", panel.border = element_blank(), panel.grid.major = element_blank(),
                      panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
 
-
 grid.newpage()
-grid.draw(rbind(ggplotGrob(p1), ggplotGrob(p2), size = "last"))
-
-grid.arrange(p1,p2,ncol=1)
+grid.draw(rbind(ggplotGrob(p1), ggplotGrob(p2)))
 
 ###VWD domains according to Pfam are from sites 78 - 231 & 317 - 466
 cyp2aligned(c(78,231,317,466))
 #106-260 & 348-498
 
+#CnL conserved cysteine are sites:
+alignment <- read.csv("~/Documents/GitHub/Cypridinidae_Emission/LuciferaseTree_dNds/results/combined_aa.csv",header=FALSE, stringsAsFactors=FALSE, colClasses = c("character"))
+cnl_seq <- alignment[alignment$V1 == "Cypridina_noctiluca_BAD08210",]
+cnl_cys <- which(cnl_seq == "C") - 1
+#
+displaysites(cnl_cys) #sites 35 and 44 change (i.e. are not conserved cysteines)
+fel_sites <- which(all_fel_table$p_value_FEL <= 0.05)
+cnl_cys[which(cnl_cys %in% fel_sites)] #cysteine residues under purifying selection #23 of 32 conserved
 
+#CnL glycosylation sites are:
+cyp2aligned(c(182,404))
+#182 -> 311
+#404 _> 435
+displaysites(435)
 
