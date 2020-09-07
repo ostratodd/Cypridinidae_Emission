a1c8957556c39dbdc5eaa86fb871cf25674bfc64
Niko <nikohensley@gmail.com>
2020-09-07 12:25:09 -0700

    Changing Fig3 labels again

--- Figure3.R
+++ Figure3.R
@@ -4,7 +4,10 @@ require(ggplot2)
 
 clean <- subset(alldata, error < 0.02 | replicate=="Vhil_tsuji" | replicate=="Cnoc_ohmiya" | replicate=="Pgra_huvard")
 quartz("Figure 3", 13, 3)
-fig3 <- ggplot(data=clean, aes(x= reorder(label, -sgMax, fun = median,), y=sgMax, fill=abbreviation)) + geom_boxplot() + geom_jitter()
+
+library(plyr)
+levels(clean$genus)[levels(clean$genus)=="Contragula"] <- "C-group"
+levels(clean$genus)[levels(clean$genus)=="Fred"] <- "'Vargula'"
 
 clean$label[clean$abbreviation == "P_EGD"] <- "P. sp. EGD (Pa)"
 clean$label[clean$abbreviation == "P_SFM"] <- "P. sp. SFM (Pa)"
@@ -20,15 +23,17 @@ clean$label[clean$abbreviation == "M_SVU"] <- "M. sp. SVU (Bz)"
 clean$label[clean$abbreviation == "P_GPH"] <- "P. sp. GPH (Ro)"
 clean$label[clean$abbreviation == "P_WLU"] <- "P. sp. WLU (Ro)"
 clean$label[clean$abbreviation == "M_LSD"] <- "M. sp. LSD (PR)"
-clean$label[clean$abbreviation == "M_IR"] <- "P. sp. IR (Ro)"
-clean$label[clean$abbreviation == "M_RD"] <- "P. sp. RD (Ro)"
-clean$label[clean$abbreviation == "M_DU"] <- "P. sp. DU (Ro)"
+clean$label[clean$abbreviation == "M_IR"] <- "M. sp. IR (Ro)"
+clean$label[clean$abbreviation == "M_RD"] <- "M. sp. RD (Ro)"
+clean$label[clean$abbreviation == "M_DU"] <- "M. sp. DU (Ro)"
 clean$label[clean$abbreviation == "V_tsu"] <- "V. tsujii (US)"
 clean$label[clean$abbreviation == "K_SPU"] <- "K. sp. SPU (PR)"
 clean$label[clean$abbreviation == "V_hil"] <- "V. hilgendorfii (Jp)"
 clean$label[clean$abbreviation == "V_hil_p"] <- "V. hilgendorfii * (Jp)"
 clean$label[clean$abbreviation == "P_gra_p"] <- "P. graminicola * (Pa)"
 clean$label[clean$abbreviation == "C_noc_p"] <- "C. noctiluca * (Jp)"
+
+fig3 <- ggplot(data=clean, aes(x= reorder(label, -sgMax, fun = median,), y=sgMax, fill=abbreviation)) + geom_boxplot() + geom_jitter()
 
 fig3 + facet_grid(cols = vars(genus), scales = "free_x", switch = "x", space = "free")  + ylab("Lambda max (nm)") + xlab("Species") +
   scale_fill_manual(values=c("#00a9ff", "#00a9ff","#007bff", "#007bff", "#007bff", "#00a9ff","#00a9ff", "#007bff","#007bff", "#007bff","#007bff", "#00a9ff","#00a9ff", "#007bff","#007bff", "#007bff","#007bff", "#007bff", "#007bff","#007bff", "#00a9ff","#007bff", "#007bff")) +
